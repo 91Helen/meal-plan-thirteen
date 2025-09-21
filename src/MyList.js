152efb13279b88cb463533b688be32a78f3112e1
91Helen <157799017+91Helen@users.noreply.github.com>
2025-09-21 23:04:45 +0500

    Update MyList.js

--- src/MyList.js
+++ src/MyList.js
@@ -1,1 +1,22 @@
+const MyList =({mealPlans, addMeal, deleteDay, selectedDay, setSelectedDay}) => {
+    return <div>
+        <div>
+        <h1>Weekly Meal Plan Ideas</h1>
+        <button className="button-add" onClick={addMeal}>Add</button>
+       
+    </div>
 
+    <div>
+        {mealPlans.map(({title,id, mealForADay}) => (
+<div className={`meal ${id ===selectedDay ? "selected" : "undefined"}`}
+onClick={() => setSelectedDay (id)}>
+    <p className="field">{title}</p>
+    <p className="field">{mealForADay.substring(0,10)}</p>
+     <button className="button-delete" onClick={() => deleteDay(id)}>Delete</button>
+</div>
+        ))}
+    </div>
+    </div>
+}
+
+export default MyList;
