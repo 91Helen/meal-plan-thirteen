f755f443f0ef3224fc7a43650ecbccb18af59e3b
91Helen <157799017+91Helen@users.noreply.github.com>
2025-09-21 23:04:19 +0500

    Update MyMealsAndIngridients.js

--- src/MyMealsAndIngridients.js
+++ src/MyMealsAndIngridients.js
@@ -1,1 +1,41 @@
+const MyMealsAndIngridients =({selectedDay, updateDay}) => {
+   
+   
+   if(!selectedDay) return <p>Plan your week ahead of time!</p>
 
+    const editMyMeal = (myInput,value) => {
+      updateDay({
+    ...selectedDay,
+     [myInput] :value
+})
+    }
+   
+
+   return <div className="whole-plan">
+     
+<input
+type="text"
+className="myInput"
+placeholder='Today is ....'
+id = 'title'
+value={selectedDay.title}
+onChange= {(e) => editMyMeal('title', e.target.value)}
+/>
+
+<textarea
+placeholder = 'Write your meal plan here'
+id="mealForADay"
+value={selectedDay.mealForADay}
+onChange= {(e) => editMyMeal('mealForADay', e.target.value)}
+/>
+
+<textarea
+placeholder='List of ingredients'
+id="ingredients"
+value = {selectedDay.ingredients}
+onChange= {(e) => editMyMeal('ingredients', e.target.value)}
+/>
+        </div>   
+}
+
+export default MyMealsAndIngridients;
